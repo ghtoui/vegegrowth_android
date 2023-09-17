fa128e3ec9b0058f1fe03727725e057fcd46c980
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-09-17 19:35:28 +0900

    mainのソースチェックだけ、ローカルではエラーなしでビルドできた

--- app/src/test/java/com/moritoui/vegegrowthapp/ExampleUnitTest.kt
+++ app/src/test/java/com/moritoui/vegegrowthapp/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.moritoui.vegegrowthapp
 
-import org.junit.Test
+import junit.framework.TestCase.assertEquals
 
-import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
