61c21eea2f63b917fbc7faba767fc53fe8c81205
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-09-17 12:07:00 +0900

    ライブラリのアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -7,12 +5,12 @@ plugins {
 }
 
 android {
-    namespace = "com.moritoui.vegegrowthapp"
+    namespace = "jp.co.fuller_inc.akita.internship2023"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.moritoui.vegegrowthapp"
-        minSdk = 24
+        applicationId = "jp.co.fuller_inc.akita.internship2023"
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -33,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -53,8 +51,7 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
