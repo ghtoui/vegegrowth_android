01f4b7c7bb19457f00ecfbafb845e9cb82912afa
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-09-17 17:04:14 +0900

    ライブラリアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "jp.co.fuller_inc.akita.internship2023"
+    namespace = "com.moritoui.vegegrowthapp"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "jp.co.fuller_inc.akita.internship2023"
+        applicationId = "com.moritoui.vegegrowthapp"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("io.coil-kt:coil:1.4.0")
+    implementation("io.coil-kt:coil-compose:2.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
