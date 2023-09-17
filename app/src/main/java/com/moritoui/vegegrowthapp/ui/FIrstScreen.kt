64a4f5ff0758c701d4c951dcaf563b8ad6295a48
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-09-17 18:00:10 +0900

    追加ボタンを押して入力ダイアグラムを表示し、入力内容を確認できるようにした

--- app/src/main/java/com/moritoui/vegegrowthapp/ui/FIrstScreen.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/FIrstScreen.kt
@@ -1,6 +1,7 @@
 package com.moritoui.vegegrowthapp.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -11,13 +12,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,7 +40,7 @@ import com.moritoui.vegegrowthapp.model.VegeItemList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FirstAppTopBar(modifier: Modifier = Modifier) {
+fun FirstAppTopBar(onClick: () -> Unit, modifier: Modifier = Modifier) {
     TopAppBar(
         title = { Text(
             text  = "管理画面",
@@ -40,7 +48,7 @@ fun FirstAppTopBar(modifier: Modifier = Modifier) {
             modifier = modifier
         ) },
         actions = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { onClick() }) {
                 Icon(
                     Icons.Filled.Add,
                     contentDescription = "追加"
@@ -53,13 +61,24 @@ fun FirstAppTopBar(modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FirstScreen() {
+    var isOpenDialog by rememberSaveable { mutableStateOf(false) }
+    var inputText by rememberSaveable { mutableStateOf("") }
+
     Scaffold(
-        topBar = { FirstAppTopBar() }
+        topBar = {
+            FirstAppTopBar(onClick = { inputText = ""; isOpenDialog = true }) }
     ) { it ->
         Box(modifier = Modifier.padding(it)) {
             ScrollView()
         }
     }
+    AddAlertWindow(
+        isOpenDialog = isOpenDialog,
+        inputText = inputText,
+        onValueChange = { inputText = it },
+        onConfirmClick = { isOpenDialog = false },
+        onDismissClick = { isOpenDialog = false }
+        )
 }
 
 @Composable
@@ -76,7 +95,7 @@ fun ScrollView(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun VegeItem(item: VegeItem ,modifier: Modifier = Modifier) {
+fun VegeItem(item: VegeItem, onClick: () -> Unit = { } ,modifier: Modifier = Modifier) {
     val categoryIcon = when(item.category) {
         VegeCategory.leaf -> painterResource(id = R.drawable.leaf)
         VegeCategory.flower -> painterResource(id = R.drawable.flower)
@@ -88,7 +107,8 @@ fun VegeItem(item: VegeItem ,modifier: Modifier = Modifier) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .height(48.dp),
+            .height(48.dp)
+            .clickable(onClick = { onClick }),
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -110,6 +130,47 @@ fun VegeItem(item: VegeItem ,modifier: Modifier = Modifier) {
             .background(Color.LightGray)
             .height(0.5.dp)
     ) {}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddAlertWindow(
+    isOpenDialog: Boolean,
+    inputText: String,
+    onValueChange: (String) -> Unit,
+    onConfirmClick: () -> Unit,
+    onDismissClick: () -> Unit
+) {
+    if (isOpenDialog) {
+        AlertDialog(
+            // ここが空だとウィンドウ外をタップしても何も起こらない
+            onDismissRequest = { },
+            title = {
+                Text(text = "追加したい野菜の名前を入力してください")
+            },
+            text = {
+                TextField(
+                    value = inputText,
+                    onValueChange = { onValueChange(it) },
+                    label = { "名前入力" },
+                    singleLine = true
+                )
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = { onConfirmClick() }
+                ) {
+                    Text("追加")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = { onDismissClick() }
+                ) {
+                    Text("キャンセル")
+                }
+            })
+    }
 }
 
 @Preview
