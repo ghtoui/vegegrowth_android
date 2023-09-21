c1bb2f8a2680943fdca66b52be48632edcdf18b1
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-09-21 10:29:49 +0900

    json解析ライブラリの追加とカメラ・ファイルアクセス設定の追加

--- app/src/main/java/com/moritoui/vegegrowthapp/model/VegeItemList.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/model/VegeItemList.kt
@@ -1,67 +1,84 @@
 package com.moritoui.vegegrowthapp.model
 
+import java.util.UUID
+
 object VegeItemList {
     fun getVegeList(): List<VegeItem> {
         return listOf(
             VegeItem(
                 name = "レタス",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "キャベツ",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "トマト",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "きゅうり",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "花",
-                category = VegeCategory.Flower
+                category = VegeCategory.Flower,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "レタス",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "キャベツ",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "トマト",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "きゅうり",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "花",
-                category = VegeCategory.Flower
+                category = VegeCategory.Flower,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "レタス",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "キャベツ",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "トマト",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "きゅうり",
-                category = VegeCategory.Leaf
+                category = VegeCategory.Leaf,
+                uuid = UUID.randomUUID()
             ),
             VegeItem(
                 name = "花",
-                category = VegeCategory.Flower
+                category = VegeCategory.Flower,
+                uuid = UUID.randomUUID()
             ),
         )
     }
--- app/src/main/java/com/moritoui/vegegrowthapp/model/VegetableRepository.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/model/VegetableRepository.kt
@@ -1,4 +1,12 @@
 package com.moritoui.vegegrowthapp.model
 
-class VegetableRepository {
-}
+import android.graphics.Bitmap
+import java.util.UUID
+
+data class VegetableRepository(
+    val uuid: UUID,
+    val name: String,
+    val size: Double,
+    var memo: String,
+    val picture: Bitmap
+)
