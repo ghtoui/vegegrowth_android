712a787869bbc1843660e342de127a9d1a34ea1d
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-09-23 19:54:58 +0900

    fileMangerクラスを作って、読み書きの処理を移した

--- app/src/main/java/com/moritoui/vegegrowthapp/model/FileManager.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/model/FileManager.kt
@@ -1,4 +1,72 @@
 package com.moritoui.vegegrowthapp.model
 
-class FileManager {
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Environment
+import android.util.Log
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.FileWriter
+import java.io.IOException
+import java.io.OutputStream
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class FileManager(
+    index: Int,
+    private val applicationContext: Context
+) {
+    private val vegeItem: VegeItem
+
+    init {
+        this.vegeItem = VegeItemList.getVegeList()[index]
+    }
+    fun readJsonData(): String? {
+        var json: String? = null
+        val jsonFileName = "${vegeItem.uuid}.json"
+        val jsonFilePath = File(applicationContext.filesDir, jsonFileName)
+        try {
+            BufferedReader(FileReader(jsonFilePath)).use { br ->
+                json = br.readLine()
+            }
+        } catch (e: IOException) {
+            Log.d("Error", "File Read Error$jsonFilePath")
+        }
+        return json
+    }
+
+    fun saveData(
+        vegeList: List<VegetableRepository>,
+        takePicImage: Bitmap?
+    ) {
+        val imageFileName = "${vegeItem.uuid}.jpg"
+        val imageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val imageFilePath = File(imageDirectory, imageFileName)
+        val outputStream: OutputStream = FileOutputStream(imageFilePath)
+        takePicImage?.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+
+        val jsonFileName = "${vegeItem.uuid}.json"
+        val jsonFilePath = File(applicationContext.filesDir, jsonFileName)
+        FileWriter(jsonFilePath).use { stream ->
+            stream.write(parseToJson(vegeList = vegeList))
+        }
+    }
+
+    private fun parseToJson(vegeList: List<VegetableRepository>): String {
+        return Json.encodeToString(vegeList)
+    }
+
+    fun parseFromJson(json: String?): MutableList<VegetableRepository> {
+        return when (json) {
+            null -> mutableListOf()
+            else -> Json.decodeFromString(json)
+        }
+    }
+
+    fun getVegeItem(): VegeItem {
+        return vegeItem
+    }
 }
--- app/src/main/java/com/moritoui/vegegrowthapp/ui/TakePictureScreenViewModel.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/TakePictureScreenViewModel.kt
@@ -2,33 +2,18 @@ package com.moritoui.vegegrowthapp.ui
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.os.Environment
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.moritoui.vegegrowthapp.model.DateFormatter
+import com.moritoui.vegegrowthapp.model.FileManager
 import com.moritoui.vegegrowthapp.model.VegeItem
-import com.moritoui.vegegrowthapp.model.VegeItemList
 import com.moritoui.vegegrowthapp.model.VegetableRepository
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileOutputStream
-import java.io.FileReader
-import java.io.FileWriter
-import java.io.IOException
-import java.io.OutputStream
 import java.time.LocalDateTime
 import java.util.UUID
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 data class TakePictureScreenUiState(
     val vegeName: String = "",
@@ -44,6 +29,8 @@ class TakePictureScreenViewModel constructor(
     private val applicationContext: Context
 ) : ViewModel() {
     private val dateFormatter = DateFormatter()
+    private val fileManager: FileManager
+
     private var vegeList: MutableList<VegetableRepository> = mutableListOf()
     private var vegeItem: VegeItem
 
@@ -63,13 +50,15 @@ class TakePictureScreenViewModel constructor(
     }
 
     init {
-        this.vegeItem = getVegeItem(index)
+        this.fileManager = FileManager(
+            index = index,
+            applicationContext = applicationContext
+        )
+        this.vegeItem = fileManager.getVegeItem()
         _uiState.update { currentState ->
             currentState.copy(vegeName = this.vegeItem.name)
         }
-        viewModelScope.launch(Dispatchers.IO) {
-            vegeList = parseFromJson(readJsonData())
-        }
+        vegeList = fileManager.parseFromJson(fileManager.readJsonData())
     }
 
     private fun updateState(
@@ -125,7 +114,7 @@ class TakePictureScreenViewModel constructor(
                 date = datetime
             )
         )
-        saveData()
+        fileManager.saveData(vegeList = vegeList, takePicImage = _uiState.value.takePicImage)
         resetState()
     }
 
@@ -143,48 +132,5 @@ class TakePictureScreenViewModel constructor(
             isSuccessInputText = isSuccessInputText,
             isBeforeInputText = false
         )
-    }
-
-    private fun saveData() {
-        val imageFileName = "${vegeItem.uuid}.jpg"
-        val imageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val imageFilePath = File(imageDirectory, imageFileName)
-        val outputStream: OutputStream = FileOutputStream(imageFilePath)
-        _uiState.value.takePicImage?.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
-
-        val jsonFileName = "${vegeItem.uuid}.json"
-        val jsonFilePath = File(applicationContext.filesDir, jsonFileName)
-        FileWriter(jsonFilePath).use { stream ->
-            stream.write(parseToJson())
-        }
-    }
-
-    private suspend fun readJsonData(): String? {
-        var json: String? = null
-        val jsonFileName = "${vegeItem.uuid}.json"
-        val jsonFilePath = File(applicationContext.filesDir, jsonFileName)
-        try {
-            BufferedReader(FileReader(jsonFilePath)).use { br ->
-                json = br.readLine()
-            }
-        } catch (e: IOException) {
-            Log.d("Error", "File Read Error$jsonFilePath")
-        }
-        return json
-    }
-
-    private fun parseToJson(): String {
-        return Json.encodeToString(vegeList)
-    }
-
-    private fun parseFromJson(json: String?): MutableList<VegetableRepository> {
-        return when (json) {
-            null -> mutableListOf<VegetableRepository>()
-            else -> Json.decodeFromString(json)
-        }
-    }
-
-    fun getVegeItem(index: Int): VegeItem {
-        return VegeItemList.getVegeList()[index]
     }
 }
