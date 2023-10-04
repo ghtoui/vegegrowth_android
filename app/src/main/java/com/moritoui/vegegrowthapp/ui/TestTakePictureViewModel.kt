1ae99da02db16a3bd516eb94faa7540d05915daa
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-10-04 17:59:37 +0900

    撮影画面の数字入力のユニットテスト追加

---
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/TestTakePictureViewModel.kt
@@ -0,0 +1,30 @@
+package com.moritoui.vegegrowthapp.ui
+
+import androidx.camera.core.ImageProxy
+import kotlinx.coroutines.flow.StateFlow
+
+class TestTakePictureViewModel: TakePictureViewModel {
+    override val uiState: StateFlow<TakePictureScreenUiState>
+        get() = uiState
+
+    override fun openRegisterDialog() { }
+
+    override fun closeRegisterDialog() { }
+
+    override fun registerVegeData() { }
+
+    override fun checkInputText(inputText: String): Boolean {
+        return super.checkInputText(inputText)
+    }
+    override fun setImage(takePic: ImageProxy) { }
+
+    override fun changeInputText(inputText: String) {
+
+    }
+
+    override fun getIndex(): Int {
+       return 0
+    }
+
+    override fun changeCameraOpenState() { }
+}
