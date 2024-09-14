20638ae0417edfa7332e7c5281c0e3ed346fc3d5
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-09-14 17:28:07 +0900

    色の更新

--- app/src/main/java/com/moritoui/vegegrowthapp/ui/theme/Theme.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/theme/Theme.kt
@@ -15,130 +15,153 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_background
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_error
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_errorContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_inverseOnSurface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_inversePrimary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_inverseSurface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onBackground
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onError
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onErrorContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onPrimary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onPrimaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onSecondary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onSecondaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onSurface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onSurfaceVariant
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onTertiary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_onTertiaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_outline
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_outlineVariant
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_primary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_primaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_scrim
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_secondary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_secondaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_surface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_surfaceTint
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_surfaceVariant
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_tertiary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_dark_tertiaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_background
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_error
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_errorContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_inverseOnSurface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_inversePrimary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_inverseSurface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onBackground
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onError
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onErrorContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onPrimary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onPrimaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onSecondary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onSecondaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onSurface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onSurfaceVariant
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onTertiary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_onTertiaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_outline
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_outlineVariant
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_primary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_primaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_scrim
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_secondary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_secondaryContainer
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_surface
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_surfaceTint
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_surfaceVariant
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_tertiary
-import com.moritoui.vegegrowthapp.ui.color.md_theme_light_tertiaryContainer
+import com.moritoui.vegegrowthapp.ui.color.backgroundDark
+import com.moritoui.vegegrowthapp.ui.color.backgroundLight
+import com.moritoui.vegegrowthapp.ui.color.errorContainerDark
+import com.moritoui.vegegrowthapp.ui.color.errorContainerLight
+import com.moritoui.vegegrowthapp.ui.color.errorDark
+import com.moritoui.vegegrowthapp.ui.color.errorLight
+import com.moritoui.vegegrowthapp.ui.color.inverseOnSurfaceDark
+import com.moritoui.vegegrowthapp.ui.color.inverseOnSurfaceLight
+import com.moritoui.vegegrowthapp.ui.color.inversePrimaryDark
+import com.moritoui.vegegrowthapp.ui.color.inversePrimaryLight
+import com.moritoui.vegegrowthapp.ui.color.inverseSurfaceDark
+import com.moritoui.vegegrowthapp.ui.color.inverseSurfaceLight
+import com.moritoui.vegegrowthapp.ui.color.onBackgroundDark
+import com.moritoui.vegegrowthapp.ui.color.onBackgroundLight
+import com.moritoui.vegegrowthapp.ui.color.onErrorContainerDark
+import com.moritoui.vegegrowthapp.ui.color.onErrorContainerLight
+import com.moritoui.vegegrowthapp.ui.color.onErrorDark
+import com.moritoui.vegegrowthapp.ui.color.onErrorLight
+import com.moritoui.vegegrowthapp.ui.color.onPrimaryContainerDark
+import com.moritoui.vegegrowthapp.ui.color.onPrimaryContainerLight
+import com.moritoui.vegegrowthapp.ui.color.onPrimaryDark
+import com.moritoui.vegegrowthapp.ui.color.onPrimaryLight
+import com.moritoui.vegegrowthapp.ui.color.onSecondaryContainerDark
+import com.moritoui.vegegrowthapp.ui.color.onSecondaryContainerLight
+import com.moritoui.vegegrowthapp.ui.color.onSecondaryDark
+import com.moritoui.vegegrowthapp.ui.color.onSecondaryLight
+import com.moritoui.vegegrowthapp.ui.color.onSurfaceDark
+import com.moritoui.vegegrowthapp.ui.color.onSurfaceLight
+import com.moritoui.vegegrowthapp.ui.color.onSurfaceVariantDark
+import com.moritoui.vegegrowthapp.ui.color.onSurfaceVariantLight
+import com.moritoui.vegegrowthapp.ui.color.onTertiaryContainerDark
+import com.moritoui.vegegrowthapp.ui.color.onTertiaryContainerLight
+import com.moritoui.vegegrowthapp.ui.color.onTertiaryDark
+import com.moritoui.vegegrowthapp.ui.color.onTertiaryLight
+import com.moritoui.vegegrowthapp.ui.color.outlineDark
+import com.moritoui.vegegrowthapp.ui.color.outlineLight
+import com.moritoui.vegegrowthapp.ui.color.outlineVariantDark
+import com.moritoui.vegegrowthapp.ui.color.outlineVariantLight
+import com.moritoui.vegegrowthapp.ui.color.primaryContainerDark
+import com.moritoui.vegegrowthapp.ui.color.primaryContainerLight
+import com.moritoui.vegegrowthapp.ui.color.primaryDark
+import com.moritoui.vegegrowthapp.ui.color.primaryLight
+import com.moritoui.vegegrowthapp.ui.color.scrimDark
+import com.moritoui.vegegrowthapp.ui.color.scrimLight
+import com.moritoui.vegegrowthapp.ui.color.secondaryContainerDark
+import com.moritoui.vegegrowthapp.ui.color.secondaryContainerLight
+import com.moritoui.vegegrowthapp.ui.color.secondaryDark
+import com.moritoui.vegegrowthapp.ui.color.secondaryLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceBrightDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceBrightLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerHighDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerHighLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerHighestDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerHighestLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerLowDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerLowLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerLowestDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceContainerLowestLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceDimDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceDimLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceLight
+import com.moritoui.vegegrowthapp.ui.color.surfaceVariantDark
+import com.moritoui.vegegrowthapp.ui.color.surfaceVariantLight
+import com.moritoui.vegegrowthapp.ui.color.tertiaryContainerDark
+import com.moritoui.vegegrowthapp.ui.color.tertiaryContainerLight
+import com.moritoui.vegegrowthapp.ui.color.tertiaryDark
+import com.moritoui.vegegrowthapp.ui.color.tertiaryLight
 
-private val LightColors =
-    lightColorScheme(
-        primary = md_theme_light_primary,
-        onPrimary = md_theme_light_onPrimary,
-        primaryContainer = md_theme_light_primaryContainer,
-        onPrimaryContainer = md_theme_light_onPrimaryContainer,
-        secondary = md_theme_light_secondary,
-        onSecondary = md_theme_light_onSecondary,
-        secondaryContainer = md_theme_light_secondaryContainer,
-        onSecondaryContainer = md_theme_light_onSecondaryContainer,
-        tertiary = md_theme_light_tertiary,
-        onTertiary = md_theme_light_onTertiary,
-        tertiaryContainer = md_theme_light_tertiaryContainer,
-        onTertiaryContainer = md_theme_light_onTertiaryContainer,
-        error = md_theme_light_error,
-        errorContainer = md_theme_light_errorContainer,
-        onError = md_theme_light_onError,
-        onErrorContainer = md_theme_light_onErrorContainer,
-        background = md_theme_light_background,
-        onBackground = md_theme_light_onBackground,
-        surface = md_theme_light_surface,
-        onSurface = md_theme_light_onSurface,
-        surfaceVariant = md_theme_light_surfaceVariant,
-        onSurfaceVariant = md_theme_light_onSurfaceVariant,
-        outline = md_theme_light_outline,
-        inverseOnSurface = md_theme_light_inverseOnSurface,
-        inverseSurface = md_theme_light_inverseSurface,
-        inversePrimary = md_theme_light_inversePrimary,
-        surfaceTint = md_theme_light_surfaceTint,
-        outlineVariant = md_theme_light_outlineVariant,
-        scrim = md_theme_light_scrim
-    )
+private val lightScheme = lightColorScheme(
+    primary = primaryLight,
+    onPrimary = onPrimaryLight,
+    primaryContainer = primaryContainerLight,
+    onPrimaryContainer = onPrimaryContainerLight,
+    secondary = secondaryLight,
+    onSecondary = onSecondaryLight,
+    secondaryContainer = secondaryContainerLight,
+    onSecondaryContainer = onSecondaryContainerLight,
+    tertiary = tertiaryLight,
+    onTertiary = onTertiaryLight,
+    tertiaryContainer = tertiaryContainerLight,
+    onTertiaryContainer = onTertiaryContainerLight,
+    error = errorLight,
+    onError = onErrorLight,
+    errorContainer = errorContainerLight,
+    onErrorContainer = onErrorContainerLight,
+    background = backgroundLight,
+    onBackground = onBackgroundLight,
+    surface = surfaceLight,
+    onSurface = onSurfaceLight,
+    surfaceVariant = surfaceVariantLight,
+    onSurfaceVariant = onSurfaceVariantLight,
+    outline = outlineLight,
+    outlineVariant = outlineVariantLight,
+    scrim = scrimLight,
+    inverseSurface = inverseSurfaceLight,
+    inverseOnSurface = inverseOnSurfaceLight,
+    inversePrimary = inversePrimaryLight,
+    surfaceDim = surfaceDimLight,
+    surfaceBright = surfaceBrightLight,
+    surfaceContainerLowest = surfaceContainerLowestLight,
+    surfaceContainerLow = surfaceContainerLowLight,
+    surfaceContainer = surfaceContainerLight,
+    surfaceContainerHigh = surfaceContainerHighLight,
+    surfaceContainerHighest = surfaceContainerHighestLight,
+)
 
-private val DarkColors =
-    darkColorScheme(
-        primary = md_theme_dark_primary,
-        onPrimary = md_theme_dark_onPrimary,
-        primaryContainer = md_theme_dark_primaryContainer,
-        onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-        secondary = md_theme_dark_secondary,
-        onSecondary = md_theme_dark_onSecondary,
-        secondaryContainer = md_theme_dark_secondaryContainer,
-        onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-        tertiary = md_theme_dark_tertiary,
-        onTertiary = md_theme_dark_onTertiary,
-        tertiaryContainer = md_theme_dark_tertiaryContainer,
-        onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-        error = md_theme_dark_error,
-        errorContainer = md_theme_dark_errorContainer,
-        onError = md_theme_dark_onError,
-        onErrorContainer = md_theme_dark_onErrorContainer,
-        background = md_theme_dark_background,
-        onBackground = md_theme_dark_onBackground,
-        surface = md_theme_dark_surface,
-        onSurface = md_theme_dark_onSurface,
-        surfaceVariant = md_theme_dark_surfaceVariant,
-        onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-        outline = md_theme_dark_outline,
-        inverseOnSurface = md_theme_dark_inverseOnSurface,
-        inverseSurface = md_theme_dark_inverseSurface,
-        inversePrimary = md_theme_dark_inversePrimary,
-        surfaceTint = md_theme_dark_surfaceTint,
-        outlineVariant = md_theme_dark_outlineVariant,
-        scrim = md_theme_dark_scrim
-    )
+private val darkScheme = darkColorScheme(
+    primary = primaryDark,
+    onPrimary = onPrimaryDark,
+    primaryContainer = primaryContainerDark,
+    onPrimaryContainer = onPrimaryContainerDark,
+    secondary = secondaryDark,
+    onSecondary = onSecondaryDark,
+    secondaryContainer = secondaryContainerDark,
+    onSecondaryContainer = onSecondaryContainerDark,
+    tertiary = tertiaryDark,
+    onTertiary = onTertiaryDark,
+    tertiaryContainer = tertiaryContainerDark,
+    onTertiaryContainer = onTertiaryContainerDark,
+    error = errorDark,
+    onError = onErrorDark,
+    errorContainer = errorContainerDark,
+    onErrorContainer = onErrorContainerDark,
+    background = backgroundDark,
+    onBackground = onBackgroundDark,
+    surface = surfaceDark,
+    onSurface = onSurfaceDark,
+    surfaceVariant = surfaceVariantDark,
+    onSurfaceVariant = onSurfaceVariantDark,
+    outline = outlineDark,
+    outlineVariant = outlineVariantDark,
+    scrim = scrimDark,
+    inverseSurface = inverseSurfaceDark,
+    inverseOnSurface = inverseOnSurfaceDark,
+    inversePrimary = inversePrimaryDark,
+    surfaceDim = surfaceDimDark,
+    surfaceBright = surfaceBrightDark,
+    surfaceContainerLowest = surfaceContainerLowestDark,
+    surfaceContainerLow = surfaceContainerLowDark,
+    surfaceContainer = surfaceContainerDark,
+    surfaceContainerHigh = surfaceContainerHighDark,
+    surfaceContainerHighest = surfaceContainerHighestDark,
+)
+
 
 @Composable
 fun VegegrowthAppTheme(
@@ -154,8 +177,8 @@ fun VegegrowthAppTheme(
                 if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
             }
 
-            darkTheme -> DarkColors
-            else -> LightColors
+            darkTheme -> darkScheme
+            else -> lightScheme
         }
     val view = LocalView.current
     if (!view.isInEditMode) {
