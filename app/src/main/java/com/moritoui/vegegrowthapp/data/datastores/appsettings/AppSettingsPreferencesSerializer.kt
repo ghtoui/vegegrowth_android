4cd17e3214db5499b544d28c2bc7703f53b80fbf
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-09-14 14:04:30 +0900

    初回起動時にマニュアル画面に遷移するように

---
+++ app/src/main/java/com/moritoui/vegegrowthapp/data/datastores/appsettings/AppSettingsPreferencesSerializer.kt
@@ -0,0 +1,32 @@
+package com.moritoui.vegegrowthapp.data.datastores.appsettings
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import java.io.InputStream
+import java.io.OutputStream
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+
+class AppSettingsPreferencesSerializer
+@Inject
+constructor() : Serializer<AppSettingsPreferences> {
+    override val defaultValue: AppSettingsPreferences = AppSettingsPreferences.initial()
+
+    override suspend fun readFrom(input: InputStream): AppSettingsPreferences {
+        try {
+            return Json.decodeFromString(
+                AppSettingsPreferences.serializer(),
+                input.readBytes().decodeToString()
+            )
+        } catch (serialization: SerializationException) {
+            throw CorruptionException("appSettingsPreferencesが読み込めませんでした", serialization)
+        }
+    }
+
+    override suspend fun writeTo(t: AppSettingsPreferences, output: OutputStream) {
+        output.write(
+            Json.encodeToString(AppSettingsPreferences.serializer(), t).encodeToByteArray()
+        )
+    }
+}
