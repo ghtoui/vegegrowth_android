dc5b003be38a2695c121b78dcbec1cf98d4cc834
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-09-16 14:09:49 +0900

    スクロールができていなかったものを修正

--- app/src/main/java/com/moritoui/vegegrowthapp/ui/initialmanual/InitialManualScreen.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/initialmanual/InitialManualScreen.kt
@@ -1,9 +1,11 @@
 package com.moritoui.vegegrowthapp.ui.initialmanual
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.moritoui.vegegrowthapp.ui.home.navigateToHome
@@ -22,7 +24,8 @@ fun InitialManualScreen(navController: NavController, viewModel: InitialManualVi
 @Composable
 private fun InitialManualScreen(modifier: Modifier = Modifier, navigateToHome: () -> Unit, finishReadManual: () -> Unit) {
     ManualContent(
-        modifier = modifier,
+        modifier = modifier
+            .padding(top = 50.dp),
         finishReadManual = {
             navigateToHome()
             finishReadManual()
