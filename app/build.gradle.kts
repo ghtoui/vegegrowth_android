02df8085efaf8ea552d6629e436aac3a2ad3ac38
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-09-17 14:50:51 +0900

    バージョンを16 -> 17へ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.moritoui.vegegrowthapp"
         minSdk = 28
         targetSdk = 34
-        versionCode = 16
-        versionName = "2.6"
+        versionCode = 17
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
