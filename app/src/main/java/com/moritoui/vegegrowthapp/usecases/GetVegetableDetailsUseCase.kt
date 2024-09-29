1788031c35cde8c6de29e509a967273550efca21
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-09-29 17:49:35 +0900

    日付順に並べるように

--- app/src/main/java/com/moritoui/vegegrowthapp/usecases/GetVegetableDetailsUseCase.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/usecases/GetVegetableDetailsUseCase.kt
@@ -5,10 +5,15 @@ import com.moritoui.vegegrowthapp.data.room.model.toVegeItemDetail
 import com.moritoui.vegegrowthapp.model.VegeItemDetail
 import javax.inject.Inject
 
+/**
+ * 詳細情報のリストを取得する
+ *
+ * 日付の順番で取得する
+ */
 class GetVegetableDetailsUseCase
 @Inject
 constructor(private val vegetableDao: VegetableDao) {
     suspend operator fun invoke(vegetableId: Int): List<VegeItemDetail> = vegetableDao.getVegetableWithDetails(vegetableId)?.details?.map {
         it.toVegeItemDetail()
-    } ?: emptyList()
+    }?.sortedBy { it.date } ?: emptyList()
 }
