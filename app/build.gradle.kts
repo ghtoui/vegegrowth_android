c48eefa4608b66a5236b6439a81d9c81b66d0aae
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-10-07 22:47:33 +0900

    ライブラリ更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,7 +128,7 @@ tasks.register<JavaExec>("ktlintFormat") {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.09.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
 
@@ -137,7 +137,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
-    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.navigation:navigation-compose:2.8.2")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     testImplementation("junit:junit:4.13.2")
