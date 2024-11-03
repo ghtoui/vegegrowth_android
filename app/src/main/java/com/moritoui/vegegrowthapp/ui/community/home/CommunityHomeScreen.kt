a730c53ea332cd503796df9ee9dd33a2ed1da9bd
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-11-03 16:46:00 +0900

    listに入れれるように

--- app/src/main/java/com/moritoui/vegegrowthapp/ui/community/home/CommunityHomeScreen.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/community/home/CommunityHomeScreen.kt
@@ -1,14 +1,13 @@
 package com.moritoui.vegegrowthapp.ui.community.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -22,6 +21,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.moritoui.vegegrowthapp.model.VegeItem
+import com.moritoui.vegegrowthapp.ui.community.home.components.CommunityListItem
 
 /**
  * 掲示板のホーム
@@ -53,17 +53,14 @@ private fun CommunityHomeScreen(
         ) {
             Text("reload")
         }
-        LazyColumn(modifier = modifier) {
+        LazyColumn(
+            modifier = modifier,
+            contentPadding = PaddingValues(horizontal = 24.dp),
+        ) {
             items(list) {
                 Column {
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Row {
-                        Text(text = it.name)
-                        Spacer(modifier = Modifier.weight(1f))
-                        Text(text = it.category.name)
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(text = it.status.name)
-                    }
+                    CommunityListItem(vegeItem = it)
+                    Spacer(modifier = Modifier.height(8.dp))
                 }
             }
         }
--- app/src/main/java/com/moritoui/vegegrowthapp/ui/community/home/components/CommunityListItem.kt
+++ app/src/main/java/com/moritoui/vegegrowthapp/ui/community/home/components/CommunityListItem.kt
@@ -3,31 +3,64 @@ package com.moritoui.vegegrowthapp.ui.community.home.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.moritoui.vegegrowthapp.model.VegeCategory
+import com.moritoui.vegegrowthapp.model.VegeItem
+import com.moritoui.vegegrowthapp.model.VegeStatus
+import com.moritoui.vegegrowthapp.model.VegeStatusMethod
+import com.moritoui.vegegrowthapp.model.getIconId
 import com.moritoui.vegegrowthapp.ui.theme.VegegrowthAppTheme
+import java.util.UUID
 
 @Composable
-fun CommunityListItem(modifier: Modifier = Modifier) {
+fun CommunityListItem(
+    vegeItem: VegeItem,
+    modifier: Modifier = Modifier
+) {
     Box(
         modifier = modifier
-            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .height(100.dp)
+            .background(
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                shape = RoundedCornerShape(12.dp),
+            ),
+        contentAlignment = Alignment.Center,
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 12.dp, vertical = 4.dp)
+                .padding(horizontal = 12.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             Text(
-                text = "",
-
+                text = "${vegeItem.name}",
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            vegeItem.category.getIconId()?.let {
+                Icon(
+                    painter = painterResource(id = it),
+                    contentDescription = null
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                painter = painterResource(id = VegeStatusMethod.getIconId(vegeItem.status)),
+                contentDescription = null
             )
         }
     }
@@ -38,7 +71,16 @@ fun CommunityListItem(modifier: Modifier = Modifier) {
 private fun CommunityListItemPreview() {
     VegegrowthAppTheme {
         Surface {
-            CommunityListItem()
+            CommunityListItem(
+                vegeItem = VegeItem(
+                    id = 0,
+                    name = "test",
+                    status = VegeStatus.Default,
+                    category = VegeCategory.Leaf,
+                    folderId = null,
+                    uuid = UUID.randomUUID().toString()
+                )
+            )
         }
     }
 }
